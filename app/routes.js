1482ae09b04701f8f5ea1eefebc2a53b589fa070
Dang Quoc Dung <dungthinhvn@gmail.com>
2016-12-27 14:55:40 -0800

    add on

--- app/routes.js
+++ app/routes.js
@@ -1,34 +1,11 @@
-module.exports = function(app,pool) {
 
-// normal routes ===============================================================
+
+module.exports = function(app) {
 
     // show the home page (will also have our login links)
     app.get("/", function(req, res) {
 
-      pool.getConnection(function(err,connection){
-          if (err) {
-            res.json({"code" : 100, "status" : "Error in connection database"});
-            return;
-          }
-
-          console.log('connected as id ' + connection.threadId);
-
-          var sql = 'select * from dichvu';
-
-          connection.query(sql,function(err,result){
-              if (err) throw err;
-
-              res.render('index',{dichvu: result});
-                  // res.json(result);
-
-          });
-
-          connection.on('error', function(err) {
-                res.json({"code" : 100, "status" : "Error in connection database"});
-                return;
-          });
-      });
-
+      res.render('about');
 
     });
 
@@ -53,6 +30,12 @@ module.exports = function(app,pool) {
     app.get("/contact", function(req, res) {
 
       res.render('contact');
+
+    });
+
+    app.get("/bien-dao-viet-nam", function(req, res) {
+
+      res.render('biendao');
 
     });
   }
--- index.js
+++ index.js
@@ -1,17 +1,74 @@
-var express = require('express')
-var mysql = require('mysql')
-
-var app = express()
+var express = require('express');
+var app = express();
 var port = process.env.PORT || 8081;
 
-var pool      =    mysql.createPool({
-    connectionLimit : 100, //important
-    host     : 'dungthinh.com',
-    user     : 'hatinh6t',
-    password : 'hatinh6t@123',
-    database : 'hatinh6t',
-    debug    :  false
+var Sequelize = require('sequelize');
+
+// include module sequelize
+var config = {
+  username: 'hatinh6t',
+  password: 'hatinh6t@123',
+  database: 'hatinh6t',
+  host: 'dungthinh.com',
+  dialect: 'mysql',
+  };
+
+// kết nối sequelize với database
+var sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+// Định nghĩa table USER
+var User = sequelize.define('user', {
+  firstName: {
+    type: Sequelize.STRING,
+    field: 'first_name' // Will result in an attribute that is firstName when user facing but first_name in the database
+  },
+  lastName: {
+    type: Sequelize.STRING
+  }
+}, {
+  freezeTableName: true // Model tableName will be the same as the model name
 });
+
+//Tao du lieu ban dau
+User.sync({force: true}).then(function () {
+  // Table created
+  return User.create({
+    firstName: 'John',
+    lastName: 'Hancock'
+  });
+});
+
+
+var DichVu = sequelize.define('dichvu2', {
+  dichvu: {
+    type: Sequelize.STRING
+  },
+  tenhinh: {
+    type: Sequelize.STRING
+  },
+  diengiai: {
+    type: Sequelize.STRING
+  },
+  fa: {
+    type: Sequelize.STRING
+  }
+}, {
+  freezeTableName: true // Model tableName will be the same as the model name
+});
+
+//Tao du lieu ban dau
+DichVu.sync({force: true}).then(function () {
+  // Table created
+  return DichVu.create(
+    {
+      dichvu: 'Tư vấn - Quản lí',
+      tenhinh: 'tuvanqlda',
+      diengiai:'xxxx',
+      fa:'xxxx'
+    }
+  );
+});
+
 
 app.use(express.static('public'));
 app.set('view engine','ejs');
@@ -21,4 +78,19 @@ app.listen(port);
 console.log('The magic happens on port ' + port);
 
 // routes ======================================================================
-require('./app/routes.js')(app, pool); // load our routes and pass in our app and fully configured passport
+//require('./app/routes.js')(app); // load our routes and pass in our app and fully configured passport
+
+app.get("/", function(req, res) {
+
+
+  DichVu.findAll().then(function (dv) {
+    res.render('index',{dichvu: dv});
+  });
+
+  User.findAll().then(function (usr) {
+    res.render('index',{doingu: usr});
+  });
+
+
+
+});
